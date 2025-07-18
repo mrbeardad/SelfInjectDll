a18156227808762f6b68c1628fbaf269e2f33bfd
Heache Bear <mrbeardad@qq.com>
2025-07-18 16:16:03 +0800

    Update DllMain.cpp

--- DllMain.cpp
+++ DllMain.cpp
@@ -1,110 +1,126 @@
-#include <memory>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <Psapi.h>
+#include <process.h>
+
 #include <string>
 
-#include <process.h>
-#include <psapi.h>
-#include <tlhelp32.h>
-
-DWORD FindProcessByName(char* szProcessName)
-{
-    DWORD cbNeeded = 0;
-    DWORD piProcesses[2048];
-    if (!EnumProcesses(piProcesses, sizeof(piProcesses), &cbNeeded)) {
-        return 0;
-    }
-
-    int iNumEnumeratedProcesses = cbNeeded / sizeof(DWORD);
-    for (int i = 0; i < iNumEnumeratedProcesses; i++) {
-        DWORD pid = piProcesses[i];
-
-        typedef std::shared_ptr<void> SafeHandle;
-        SafeHandle hProcess = SafeHandle(OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid), [](HANDLE hHandleToClose) {
-            if (hHandleToClose != NULL) {
-                CloseHandle(hHandleToClose);
-            }
-        });
-
-        if (hProcess != NULL) {
-            CHAR szProcessPath[MAX_PATH] {};
-            DWORD size = MAX_PATH;
-            if (!QueryFullProcessImageNameA(hProcess.get(), 0, szProcessPath, &size)) {
-                continue;
-            }
-            char* sep = ::strrchr(szProcessPath, '\\');
-            if (!sep) {
-                continue;
-            }
-            if (::_stricmp(sep + 1, szProcessName) == 0) {
-                return pid;
-            }
-        }
-    }
-
-    return 0;
-}
-
-LRESULT CALLBACK GetMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
+static LRESULT CALLBACK GetMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 
-extern "C" __declspec(dllexport) void CALLBACK Inject(HWND hwnd,
-    HINSTANCE hinst,
-    LPSTR pszCmdLine,
-    int nCmdShow)
+extern "C" __declspec(dllexport) void CALLBACK Inject(HWND hwnd, HINSTANCE hinst, LPSTR pszCmdLine, int nCmdShow)
 {
-    auto hb = &__ImageBase;
-    DWORD pid = FindProcessByName(pszCmdLine);
-    if (pid == 0) {
-        printf("ERROR: process %s not found\n", pszCmdLine);
+//#define TARGET_PROCESS_NAME "TargetProcessName.exe"
+    // Get wstring processName
+#ifdef TARGET_PROCESS_NAME
+    LPCSTR szProcessName = TARGET_PROCESS_NAME;
+    size_t length = ::strlen(TARGET_PROCESS_NAME);
+#else
+    LPCSTR szProcessName = pszCmdLine;
+    size_t length = ::strlen(pszCmdLine);
+#endif
+    std::wstring processName;
+    int size = ::MultiByteToWideChar(CP_ACP, 0, szProcessName, length, NULL, 0);
+    processName.resize(size);
+    ::MultiByteToWideChar(CP_ACP, 0, szProcessName, length, processName.data(), size);
+
+    // Search the process
+    DWORD pid = 0;
+    {
+        HANDLE snapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+        if (snapshot == INVALID_HANDLE_VALUE)
+        {
+            return;
+        }
+        PROCESSENTRY32W entry{sizeof(PROCESSENTRY32W)};
+        if (::Process32FirstW(snapshot, &entry))
+        {
+            do
+            {
+                if (entry.th32ProcessID > 0 && ::_wcsicmp(entry.szExeFile, processName.c_str()) == 0)
+                {
+                    pid = entry.th32ProcessID;
+                    break;
+                }
+            } while (::Process32NextW(snapshot, &entry));
+        }
+        ::CloseHandle(snapshot);
+    }
+    if (pid == 0)
+    {
+        ::wprintf(L"ERROR: process %s not found\n", processName.c_str());
         return;
     }
-    HANDLE h = ::CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-    if (h != INVALID_HANDLE_VALUE) {
-        THREADENTRY32 te;
-        te.dwSize = sizeof(te);
-        if (Thread32First(h, &te)) {
-            do {
-                if (te.dwSize >= FIELD_OFFSET(THREADENTRY32, th32OwnerProcessID) + sizeof(te.th32OwnerProcessID) && te.th32OwnerProcessID == pid) {
-                    if (::SetWindowsHookEx(WH_GETMESSAGE, (HOOKPROC)GetMsgProc, (HINSTANCE)&__ImageBase, te.th32ThreadID)) {
+
+    // Search the UI thread then inject Windows Hook
+    {
+        HANDLE snapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+        if (snapshot == INVALID_HANDLE_VALUE)
+        {
+            return;
+        }
+        THREADENTRY32 entry{sizeof(THREADENTRY32)};
+        if (::Thread32First(snapshot, &entry))
+        {
+            do
+            {
+                if (entry.th32OwnerProcessID == pid)
+                {
+                    if (::SetWindowsHookExW(WH_GETMESSAGE, (HOOKPROC)GetMsgProc, (HINSTANCE)&__ImageBase, entry.th32ThreadID))
+                    {
                         // Sleep for a while to insure there is enough time to inject dll
-                        for (int i = 0; i < 10; i++) {
-                            ::PostThreadMessage(te.th32ThreadID, WM_NULL, NULL, NULL);
-                            Sleep(500);
+                        for (int i = 0; i < 3; i++)
+                        {
+                            ::PostThreadMessageW(entry.th32ThreadID, WM_NULL, NULL, NULL);
+                            ::Sleep(500);
                         }
                         break;
                     }
                 }
-                te.dwSize = sizeof(te);
-            } while (Thread32Next(h, &te));
+            } while (::Thread32Next(snapshot, &entry));
         }
-        CloseHandle(h);
+        ::CloseHandle(snapshot);
     }
 }
 
-BOOL IsRunDll32()
+static BOOL IsRunDll32()
 {
-    CHAR szMainModulePath[MAX_PATH];
-    DWORD dwMainModulePathLength = ::GetModuleFileNameA(NULL, szMainModulePath, sizeof(szMainModulePath));
-    return dwMainModulePathLength > 13 && ::_stricmp(szMainModulePath + dwMainModulePathLength - 13, "\\rundll32.exe") == 0;
+    WCHAR wszBaseName[MAX_PATH]{};
+    ::GetModuleBaseNameW(::GetCurrentProcess(), NULL, wszBaseName, sizeof(wszBaseName) / sizeof(WCHAR));
+    return ::_wcsicmp(wszBaseName, L"rundll32.exe") == 0;
 }
 
 unsigned WINAPI MainThread(LPVOID)
 {
-    ::system("echo My dll has been loaded. && pause");
+    __try
+    {
+        for (int count = 0; count < 10; count++)
+        {
+            ::system("echo My dll has been loaded. && pause");
+            break;
+            ::Sleep(500);
+        }
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        ::OutputDebugStringW(L"===== EXCEPTION ERROR");
+    }
     return 0;
 }
 
 BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
 {
-    switch (dwReason) {
+    switch (dwReason)
+    {
     case DLL_PROCESS_ATTACH:
         ::DisableThreadLibraryCalls(hMod);
-        if (!IsRunDll32()) {
+        if (!IsRunDll32())
+        {
             // Dll will been unloaded after the thread end
-            // ::GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCSTR)hMod, &hMod);
             ::_beginthreadex(NULL, 0, MainThread, NULL, 0, NULL);
         }
         break;
